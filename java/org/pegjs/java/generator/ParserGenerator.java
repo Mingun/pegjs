350f2ef7c1d3d97bcd6a7c697de4a5244e05f5a3
Mingun <alexander_sergey@mail.ru>
2014-03-15 15:48:37 +0600

    Расширен API генератора парсера для возможности задания базового класса или интерфейса, который должен реализовывать возвращаемый парсер. Указанный класс может хранить дополнительное состояние и реализовывать сервисные методы и методы действий/предикатов, которое необходимы парсеру для разбора.

--- java/org/pegjs/java/generator/ParserGenerator.java
+++ java/org/pegjs/java/generator/ParserGenerator.java
@@ -41,11 +41,12 @@ public final class ParserGenerator extends ClassLoader {
     private static final String AutoClassNamePrefix = "org.pegjs.java.generated.Parser";
 
     //<editor-fold defaultstate="collapsed" desc="Публичный интерфейс">
+    //<editor-fold defaultstate="collapsed" desc="Не рекомендуемые к использованию методы">
     /**
      * Генерирует и загружает класс с автоматически сгенерированным именем для
      * разбора указанной грамматики. Класс генерируется в пакете {@code org.pegjs.java.generated}
      * и имеет имя вида {@code Parser<16-значный UUID>}.
-     * @param <T> Тип результата, возвращаемого генерируемым парсером.
+     * @param <R> Тип результата, возвращаемого генерируемым парсером.
      * @param grammar Описание грамматики (см. <a href="https://github.com/Mingun/pegjs/tree/master#grammar-syntax-and-semantics">синтаксис</a>).
      * @return Класс парсера, загруженный {@linkplain ParserGenerator}-ом.
      * @deprecated Используйте этот метод только для быстрого тестирования. Во избежание
@@ -53,14 +54,14 @@ public final class ParserGenerator extends ClassLoader {
      * @see #generate(java.lang.CharSequence, java.lang.String).
      */
     @Deprecated
-    public <T> IParser<T> build(CharSequence grammar) {
+    public <R> IParser<R> build(CharSequence grammar) {
         return build(grammar, autoClassName());
     }
     /**
      * Генерирует и загружает класс с автоматически сгенерированным именем для
      * разбора указанной грамматики. Класс генерируется в пакете {@code org.pegjs.java.generated}
      * и имеет имя вида {@code Parser<16-значный UUID>}.
-     * @param <T> Тип результата, возвращаемого генерируемым парсером.
+     * @param <R> Тип результата, возвращаемого генерируемым парсером.
      * @param ast Дерево грамматики.
      * @return Класс парсера, загруженный {@linkplain ParserGenerator}-ом.
      * @deprecated Используйте этот метод только для быстрого тестирования. Во избежание
@@ -68,59 +69,151 @@ public final class ParserGenerator extends ClassLoader {
      * @see #generate(java.lang.CharSequence, java.lang.String).
      */
     @Deprecated
-    public <T> IParser<T> build(GrammarNode ast) {
+    public <R> IParser<R> build(GrammarNode ast) {
         return build(ast, autoClassName());
     }
     /**
+     * Генерирует и загружает класс с указанным именем, унаследованный от переданного
+     * класса, для разбора указанной грамматики. Класс генерируется в пакете {@code org.pegjs.java.generated}
+     * и имеет имя вида {@code Parser<16-значный UUID>}.
+     * @param <P> Тип возвращаемого парсера. Этот тип является наследником указанного
+     *        в параметрах класса и реализует интерфейс {@link IParser}.
+     * @param <R> Тип возвращаемого сгенерированным парсером значения.
+     * @param grammar Описание грамматики (см. <a href="https://github.com/Mingun/pegjs/tree/master#grammar-syntax-and-semantics">синтаксис</a>).
+     * @param base Базовый класс для генерируемого парсера. Как правило, содержит
+     *        дополнительные переменные, описывающие состояние парсера, а также
+     *        содержит реализацию действий и предикатов, которые будут выполняться
+     *        во время разбора.
+     * @return Класс парсера, загруженный {@linkplain ParserGenerator}-ом.
+     * @deprecated Используйте этот метод только для быстрого тестирования. Во избежание
+     *             коллизий, сами назначайте имена генерируемым классам.
+     */
+    @Deprecated
+    public <P extends IParser<? extends R>, R> P build(CharSequence grammar, Class<? super P> base) {
+        return build(grammar, autoClassName(), base);
+    }
+    /**
+     * Генерирует и загружает класс с указанным именем, унаследованный от переданного
+     * класса, для разбора указанной грамматики. Класс генерируется в пакете {@code org.pegjs.java.generated}
+     * и имеет имя вида {@code Parser<16-значный UUID>}.
+     * @param <P> Тип возвращаемого парсера. Этот тип является наследником указанного
+     *        в параметрах класса и реализует интерфейс {@link IParser}.
+     * @param <R> Тип возвращаемого сгенерированным парсером значения.
+     * @param ast Дерево грамматики.
+     * @param base Базовый класс для генерируемого парсера. Как правило, содержит
+     *        дополнительные переменные, описывающие состояние парсера, а также
+     *        содержит реализацию действий и предикатов, которые будут выполняться
+     *        во время разбора.
+     * @return Класс парсера, загруженный {@linkplain ParserGenerator}-ом.
+     * @deprecated Используйте этот метод только для быстрого тестирования. Во избежание
+     *             коллизий, сами назначайте имена генерируемым классам.
+     */
+    @Deprecated
+    public <P extends IParser<? extends R>, R> P build(GrammarNode ast, Class<? super P> base) {
+        return build(ast, autoClassName(), base);
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Генерация класса парсера">
+    /**
      * Генерирует и загружает класс с указанным именем для разбора указанной грамматики.
-     * @param <T> Тип результата, возвращаемого генерируемым парсером.
+     * @param <R> Тип результата, возвращаемого генерируемым парсером.
      * @param grammar Описание грамматики (см. <a href="https://github.com/Mingun/pegjs/tree/master#grammar-syntax-and-semantics">синтаксис</a>).
      * @param className Полностью квалифицированное имя генерируемого класса,
      *        реализующего разбор грамматики, например, {@code org.pegjs.java.generator.Parser}.
      * @return Класс парсера, загруженный {@linkplain ParserGenerator}-ом.
      */
-    public <T> IParser<T> build(CharSequence grammar, String className) {
-        byte[] byteCode = generate(grammar, className);
+    public <R> IParser<R> build(CharSequence grammar, String className) {
+        final byte[] byteCode = generate(grammar, className);
         try {
-            return (IParser<T>)defineClass(className, byteCode, 0, byteCode.length).newInstance();
+            return (IParser<R>)defineClass(className, byteCode, 0, byteCode.length).newInstance();
         } catch (InstantiationException ex) {
             // Невозможно - у парсера есть публичный конструктор без аргументов
-            final InternalError e = new InternalError();
-            e.initCause(ex);
-            throw e;
+            throw (InternalError)new InternalError().initCause(ex);
         } catch (IllegalAccessException ex) {
             // Невозможно - у парсера есть публичный конструктор без аргументов
-            final InternalError e = new InternalError();
-            e.initCause(ex);
-            throw e;
+            throw (InternalError)new InternalError().initCause(ex);
         }
     }
     /**
      * Генерирует и загружает класс с указанным именем для разбора указанной грамматики.
-     * @param <T> Тип результата, возвращаемого генерируемым парсером.
+     * @param <R> Тип результата, возвращаемого генерируемым парсером.
      * @param ast Дерево грамматики.
      * @param className Полностью квалифицированное имя генерируемого класса,
      *        реализующего разбор грамматики, например, {@code org.pegjs.java.generator.Parser}.
      * @return Класс парсера, загруженный {@linkplain ParserGenerator}-ом.
      */
-    public <T> IParser<T> build(GrammarNode ast, String className) {
-        byte[] byteCode = generate(ast, className);
+    public <R> IParser<R> build(GrammarNode ast, String className) {
+        final byte[] byteCode = generate(ast, className);
         try {
-            return (IParser<T>)defineClass(className, byteCode, 0, byteCode.length).newInstance();
+            return (IParser<R>)defineClass(className, byteCode, 0, byteCode.length).newInstance();
         } catch (InstantiationException ex) {
             // Невозможно - у парсера есть публичный конструктор без аргументов
-            final InternalError e = new InternalError();
-            e.initCause(ex);
-            throw e;
+            throw (InternalError)new InternalError().initCause(ex);
         } catch (IllegalAccessException ex) {
             // Невозможно - у парсера есть публичный конструктор без аргументов
-            final InternalError e = new InternalError();
-            e.initCause(ex);
-            throw e;
+            throw (InternalError)new InternalError().initCause(ex);
         }
     }
     /**
-     * Разбирает указанную грамматику и строит по ней парсер.
+     * Генерирует и загружает класс с указанным именем, унаследованный от переданного
+     * класса, для разбора указанной грамматики.
+     * @param <P> Тип возвращаемого парсера. Этот тип является наследником указанного
+     *        в параметрах класса и реализует интерфейс {@link IParser}.
+     * @param <R> Тип возвращаемого сгенерированным парсером значения.
+     * @param grammar Описание грамматики (см. <a href="https://github.com/Mingun/pegjs/tree/master#grammar-syntax-and-semantics">синтаксис</a>).
+     * @param className Полностью квалифицированное имя генерируемого класса,
+     *        реализующего разбор грамматики, например, {@code org.pegjs.java.generator.Parser}.
+     * @param base Базовый класс для генерируемого парсера. Как правило, содержит
+     *        дополнительные переменные, описывающие состояние парсера, а также
+     *        содержит реализацию действий и предикатов, которые будут выполняться
+     *        во время разбора.
+     * @return Класс парсера, загруженный {@linkplain ParserGenerator}-ом.
+     */
+    public <P extends IParser<? extends R>, R> P build(CharSequence grammar, String className, Class<? super P> base) {
+        final byte[] byteCode = generate(grammar, className, base);
+        try {
+            return (P)defineClass(className, byteCode, 0, byteCode.length).newInstance();
+        } catch (InstantiationException ex) {
+            // Невозможно - у парсера есть публичный конструктор без аргументов
+            throw (InternalError)new InternalError().initCause(ex);
+        } catch (IllegalAccessException ex) {
+            // Невозможно - у парсера есть публичный конструктор без аргументов
+            throw (InternalError)new InternalError().initCause(ex);
+        }
+    }
+    /**
+     * Генерирует и загружает класс с указанным именем, унаследованный от переданного
+     * класса, для разбора указанной грамматики.
+     * @param <P> Тип возвращаемого парсера. Этот тип является наследником указанного
+     *        в параметрах класса и реализует интерфейс {@link IParser}.
+     * @param <R> Тип возвращаемого сгенерированным парсером значения.
+     * @param ast Дерево грамматики.
+     * @param className Полностью квалифицированное имя генерируемого класса,
+     *        реализующего разбор грамматики, например, {@code org.pegjs.java.generator.Parser}.
+     * @param base Базовый класс для генерируемого парсера. Как правило, содержит
+     *        дополнительные переменные, описывающие состояние парсера, а также
+     *        содержит реализацию действий и предикатов, которые будут выполняться
+     *        во время разбора.
+     * @return Класс парсера, загруженный {@linkplain ParserGenerator}-ом.
+     */
+    public <P extends IParser<? extends R>, R> P build(GrammarNode ast, String className, Class<? super P> base) {
+        final byte[] byteCode = generate(ast, className, base);
+        try {
+            return (P)defineClass(className, byteCode, 0, byteCode.length).newInstance();
+        } catch (InstantiationException ex) {
+            // Невозможно - у парсера есть публичный конструктор без аргументов
+            throw (InternalError)new InternalError().initCause(ex);
+        } catch (IllegalAccessException ex) {
+            // Невозможно - у парсера есть публичный конструктор без аргументов
+            throw (InternalError)new InternalError().initCause(ex);
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Генерация байт-кода класса парсера">
+    /**
+     * Разбирает указанную грамматику и генерирует байткод парсера для ее разбора.
      * @param grammar Описание грамматики (см. <a href="https://github.com/Mingun/pegjs/tree/master#grammar-syntax-and-semantics">синтаксис</a>).
      * @param className Полностью квалифицированное имя генерируемого класса,
      *        реализующего разбор грамматики, например, {@code org.pegjs.java.generator.Parser}.
@@ -133,7 +226,7 @@ public final class ParserGenerator extends ClassLoader {
         return generate(ast, className);
     }
     /**
-     * Генерирует байткод класса парсера с указанным именем.
+     * Генерирует байткод класса парсера с указанным именем, для разбора указанной грамматики.
      * @param ast Описание грамматики, для которой генерируется парсер.
      * @param className Полностью квалифицированное имя генерируемого класса,
      *        реализующего разбор грамматики, например, {@code org.pegjs.java.generator.Parser}.
@@ -173,6 +266,39 @@ public final class ParserGenerator extends ClassLoader {
         }
         return fm.classData(className);
     }
+    /**
+     * Разбирает указанную грамматику и генерирует байткод парсера для ее разбора
+     * унаследованный от переданного класса.
+     * @param grammar Описание грамматики (см. <a href="https://github.com/Mingun/pegjs/tree/master#grammar-syntax-and-semantics">синтаксис</a>).
+     * @param className Полностью квалифицированное имя генерируемого класса,
+     *        реализующего разбор грамматики, например, {@code org.pegjs.java.generator.Parser}.
+     * @param base Базовый класс для генерируемого парсера. Как правило, содержит
+     *        дополнительные переменные, описывающие состояние парсера, а также
+     *        содержит реализацию действий и предикатов, которые будут выполняться
+     *        во время разбора.
+     * @return Класс парсера, загруженный {@linkplain ParserGenerator}-ом.
+     */
+    public static byte[] generate(CharSequence grammar, String className, Class<?> base) {
+        final Parser p = new Parser();
+        final GrammarNode ast = (GrammarNode)p.parse(grammar);
+        return generate(ast, className, base);
+    }
+    /**
+     * Генерирует байткод класса парсера с указанным именем, унаследованный от 
+     * переданного класса, для разбора указанной грамматики.
+     * @param ast Дерево грамматики.
+     * @param className Полностью квалифицированное имя генерируемого класса,
+     *        реализующего разбор грамматики, например, {@code org.pegjs.java.generator.Parser}.
+     * @param base Базовый класс для генерируемого парсера. Как правило, содержит
+     *        дополнительные переменные, описывающие состояние парсера, а также
+     *        содержит реализацию действий и предикатов, которые будут выполняться
+     *        во время разбора.
+     * @return Класс парсера, загруженный {@linkplain ParserGenerator}-ом.
+     */
+    public static byte[] generate(GrammarNode ast, String className, Class<?> base) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    //</editor-fold>
     //</editor-fold>
     private static String autoClassName() {
         return AutoClassNamePrefix + UUID.randomUUID().toString().replace("-", "");
