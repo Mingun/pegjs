d459ff2e37f1b9d240e19308c1078734188a6399
Mingun <alexander_sergey@mail.ru>
2014-11-29 23:07:23 +0500

    Issue #38 (import): Add pass for include (at AST level) one grammar to another.
    Conflicts:
    	lib/compiler/index.js
    	package.json

---
+++ test/unit/compiler/passes/include-grammars.spec.js
@@ -0,0 +1,239 @@
+"use strict";
+
+let chai = require("chai");
+let helpers = require("./helpers");
+let pass = require("../../../../lib/compiler/passes/include-grammars");
+let Resolver = require("../../../../lib/compiler/resolver.js");
+
+chai.use(helpers);
+
+let expect = chai.expect;
+
+describe("compiler pass |includeGrammars|", function() {
+  describe("add rules from imported grammar to grammar AST", function() {
+    let options = {
+      resolver: new Resolver(function() {
+        return {
+          path: "",
+          data: [
+            "rule1 = 'a'",
+            "rule2 = 'b'",
+            "rule3 = 'c'"
+          ].join("\n")
+        };
+      })
+    };
+    it("with default rule ref", function() {
+      expect(pass).to.changeAST(
+        [
+          "#inc = ''",
+          "start = #inc"
+        ].join("\n"),
+        {
+          rules: [
+            {
+              name:       "inc$rule1",
+              expression: { type: "literal", value: "a" }
+            },
+            {
+              name:       "inc$rule2",
+              expression: { type: "literal", value: "b" }
+            },
+            {
+              name:       "inc$rule3",
+              expression: { type: "literal", value: "c" }
+            },
+            {
+              name:       "start",
+              expression: { type: "rule_ref", namespace: null, name: "inc$rule1" }
+            }
+          ]
+        },
+        options
+      );
+    });
+    it("with explicit specified rule ref", function() {
+      expect(pass).to.changeAST(
+        [
+          "#inc = ''",
+          "start = #inc:rule2"
+        ].join("\n"),
+        {
+          rules: [
+            {
+              name:       "inc$rule1",
+              expression: { type: "literal", value: "a" }
+            },
+            {
+              name:       "inc$rule2",
+              expression: { type: "literal", value: "b" }
+            },
+            {
+              name:       "inc$rule3",
+              expression: { type: "literal", value: "c" }
+            },
+            {
+              name:       "start",
+              expression: { type: "rule_ref", namespace: null, name: "inc$rule2" }
+            }
+          ]
+        },
+        options
+      );
+    });
+  });
+
+  describe("allow import one grammar multiply times with distinct aliases", function() {
+    it("with default rule ref", function() {
+      expect(pass).to.changeAST(
+        [
+          "#inc1 = ''",
+          "#inc2 = ''",
+          "start = #inc1 #inc2"
+        ].join("\n"),
+        {
+          rules: [
+            {
+              name:       "inc1$rule",
+              expression: { type: "literal", value: "a" }
+            },
+            {
+              name:       "inc2$rule",
+              expression: { type: "literal", value: "a" }
+            },
+            {
+              name:       "start",
+              expression: {
+                elements: [
+                  { type: "rule_ref", namespace: null, name: "inc1$rule" },
+                  { type: "rule_ref", namespace: null, name: "inc2$rule" }
+                ]
+              }
+            }
+          ]
+        },
+        { resolver: new Resolver(function() { return { path: "", data: "rule = 'a'" }; }) }
+      );
+    });
+    it("with explicit specified rule ref", function() {
+      expect(pass).to.changeAST(
+        [
+          "#inc1 = ''",
+          "#inc2 = ''",
+          "start = #inc1:rule1 #inc2:rule2"
+        ].join("\n"),
+        {
+          rules: [
+            {
+              name:       "inc1$rule1",
+              expression: { type: "literal", value: "a" }
+            },
+            {
+              name:       "inc1$rule2",
+              expression: { type: "literal", value: "b" }
+            },
+            {
+              name:       "inc2$rule1",
+              expression: { type: "literal", value: "a" }
+            },
+            {
+              name:       "inc2$rule2",
+              expression: { type: "literal", value: "b" }
+            },
+            {
+              name:       "start",
+              expression: {
+                elements: [
+                  { type: "rule_ref", namespace: null, name: "inc1$rule1" },
+                  { type: "rule_ref", namespace: null, name: "inc2$rule2" }
+                ]
+              }
+            }
+          ]
+        },
+        { resolver: new Resolver(function() { return { path: "", data: "rule1 = 'a';rule2 = 'b'" }; }) }
+      );
+    });
+  });
+
+  describe("recursive include rules from included grammars", function() {
+    let options = {
+      resolver: new Resolver(function(path) {
+        return {
+          path: path,
+          data: [
+            path.length === 0 ? "#inc = 'second'" : "",
+            "rule1 = 'a'",
+            "rule2 = 'b'"
+          ].join("\n")
+        };
+      })
+    };
+    it("with default rule ref", function() {
+      expect(pass).to.changeAST(
+        [
+          "#inc = ''",
+          "start = #inc"
+        ].join("\n"),
+        {
+          rules: [
+            {
+              name:       "inc$inc$rule1",
+              expression: { type: "literal", value: "a" }
+            },
+            {
+              name:       "inc$inc$rule2",
+              expression: { type: "literal", value: "b" }
+            },
+            {
+              name:       "inc$rule1",
+              expression: { type: "literal", value: "a" }
+            },
+            {
+              name:       "inc$rule2",
+              expression: { type: "literal", value: "b" }
+            },
+            {
+              name:       "start",
+              expression: { type: "rule_ref", namespace: null, name: "inc$rule1" }
+            }
+          ]
+        },
+        options
+      );
+    });
+    it("with explicit specified rule ref", function() {
+      expect(pass).to.changeAST(
+        [
+          "#inc = ''",
+          "start = #inc:rule2"
+        ].join("\n"),
+        {
+          rules: [
+            {
+              name:       "inc$inc$rule1",
+              expression: { type: "literal", value: "a" }
+            },
+            {
+              name:       "inc$inc$rule2",
+              expression: { type: "literal", value: "b" }
+            },
+            {
+              name:       "inc$rule1",
+              expression: { type: "literal", value: "a" }
+            },
+            {
+              name:       "inc$rule2",
+              expression: { type: "literal", value: "b" }
+            },
+            {
+              name:       "start",
+              expression: { type: "rule_ref", namespace: null, name: "inc$rule2" }
+            }
+          ]
+        },
+        options
+      );
+    });
+  });
+});
